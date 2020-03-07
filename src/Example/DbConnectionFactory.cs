567c39d4ed534a7efcb42ce86d88737577f9339f
Lex Galante <alex_gm_100@hotmail.com>
2020-03-07 11:37:37 -0300

    *

---
+++ src/Example/DbConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    public static class DbConnectionFactory
+    {
+        public static DbConnection GetConnection<T>() where T: class
+        {
+            if (typeof(T) == typeof(SqlServerConnection))
+                return new SqlServerConnection();
+            else
+                return new OracleConnection();
+        }
+    }
+}
---
+++ src/Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqlServerConnection = DbConnectionFactory.GetConnection<SqlServerConnection>();
+            Console.WriteLine(sqlServerConnection);
+
+            var oracleServerConnection = DbConnectionFactory.GetConnection<OracleConnection>();
+            Console.WriteLine(oracleServerConnection);
+        }
+    }
+}
